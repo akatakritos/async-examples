4060508897cf1c444973e3efc962b00512221ea7
Matt Burke <mburke@redventures.com>
2013-07-03 12:38:11 -0400

    clean up each example

--- each-examples.js
+++ each-examples.js
@@ -13,27 +13,62 @@ function slowIterator( item, callback ) {
 
 var items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+/**
+ * Example of async.each
+ *
+ * The slowIterator function will be called for each item in the input array.
+ * Becasuse each callback can take a different length of time, order cannot
+ * be guaranteed.
+ * 
+ * @param  {Function} callback Called when example is comeplte
+ */
+function eachExample( callback ) {
+	console.log("async.each");
+	async.each( items, slowIterator, function( err ) {
+		console.log();
+		callback(err);
+	});
+}
+
+/**
+ * Example of async.eachSeries
+ *
+ * The slowIterator callback will be called for each item in the input array.
+ * However, slowIterator will not becalled until the previous invocation has
+ * completed. Order is guaranteed.
+ * 
+ * @param  {Function} callback Called when example is complete
+ */
+function eachSeriesExample( callback ) {
+	console.log("async.eachSeries");
+	async.eachSeries( items, slowIterator, function(err) {
+		console.log();
+		callback(err);
+	});
+}
+
+
+/**
+ * Example of async.eachLimit
+ *
+ * The slowIterator callback will be called for each item in the input array.
+ * However, only a limited number of invocations will be allowed at once.
+ * 
+ * @param  {Function} callback Called when the example is complete
+ * @return {[type]}            [description]
+ */
+function eachLimitExample( callback ){
+	console.log("async.eachLimit");
+	async.eachLimit( items, 3, slowIterator, function(err) {
+		console.log();
+		callback(err);
+	});
+}
+
 async.series(
 	[
-		function( cb ) {
-			console.log();
-			console.log( "async.each - The iterator callbacks are called asyncronously.");
-			console.log( "They do not print in order because each callback takes a different length of time.");
-			async.each( items, slowIterator, function( err ) { cb(err); });
-		},
-
-		function( cb ) {
-			console.log();
-			console.log( "async.eachSeries - every iterator callback is called asyncronously, but not until the previous callback has completed.");
-			console.log( "They will print in order this time. Notice that it also takes much longer to complete.");
-			async.eachSeries( items, slowIterator, function( err ) { cb(err); });
-		},
-
-		function( cb ) {
-			console.log();
-			console.log( "async.eachLimit - only x number of callbacks are allowed to run at once." );
-			console.log( "Order is not maintained.");
-			async.eachLimit( items, 3, slowIterator, function( err) { cb(err); });
-		}
+		eachExample,
+		eachSeriesExample,
+		eachLimitExample
 	]
 );
